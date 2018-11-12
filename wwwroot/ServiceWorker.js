bb30eee7babe1807a57b712589b0b69fc075e68d
Dante Pasionek <dante.pasionek1@gmail.com>
2018-11-11 21:41:29 -0700

    Added service worker

---
+++ wwwroot/ServiceWorker.js
@@ -0,0 +1,42 @@
+const CACHE = 'snwflk-0.0.1';
+const TITLE = '[ServiceWorker] ';
+const EXPECTED_CACHE = [CACHE];
+self.addEventListener('install', event => {
+    var index = new Request('../snwflk/app.html');
+    event.waitUntil(
+        fetch(index).then(response => {
+            return caches.open(CACHE).then(cache => {       // This returns a promise, then caches page
+                console.log(TITLE + 'Cached: ' + response.url);
+                return cache.put(index, response);                  // Actually caches page
+            });
+        }).catch(error => console.log(TITLE + 'An error occured: ' + error))
+    );
+});
+
+// Activate
+// Clean up caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (!EXPECTED_CACHE.includes(key)) {
+                    return caches.delete(key);
+                }
+            })
+        ))//.then(() => console.log(TITLE + 'Cache cleaned; Ready to handle fetches'))
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+};
