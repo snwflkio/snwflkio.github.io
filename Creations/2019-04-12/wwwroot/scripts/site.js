c63baaaa66e78f44032a272a67642c7039065393
Dante Pasionek <dante.pasionek1@gmail.com>
2019-04-12 00:00:42 +0000

    First

---
+++ Creations/2019-04-12/wwwroot/scripts/site.js
@@ -0,0 +1,38 @@
+var val = ""
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  var style = window.getComputedStyle(ev.target, null);
+  var topPos = style.getPropertyValue("top");
+  var leftPos = style.getPropertyValue("left");
+  if(topPos == "auto") topPos = 0;
+  if(leftPos == "auto") topPos = 0;
+  console.log(topPos + " " + leftPos)
+  var data = ev.target.id + "," + topPos + "," + leftPos
+  ev.dataTransfer.setData("text", data);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text").split(',');
+  var design = document.getElementById("PLACEHOLDER");
+  var width = window.getComputedStyle(design).width;
+  console.log(data)
+  var item = document.getElementById(data[0]);
+  ev.target.appendChild(item);
+  item.style.position = "relative";
+  
+  item.style.left = ((ev.clientX - width.replace("px","")) + 'px');
+  item.style.top = (ev.clientY + 'px');
+
+
+   document.getElementById(data[0].split('-')[0] + '-setter').style["display"] = "block";
+   return false;
+}
+
+function getInfo()
+{
+	return val;
+}
